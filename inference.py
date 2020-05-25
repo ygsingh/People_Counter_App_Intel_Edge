b46a1b0fbacbc5ad191bb070723e16b6fd5e35b0
Yogendra Singh <ygsingh101@gmail.com>
2020-05-24 23:46:15 -0400

    Upload main file

--- inference.py
+++ inference.py
@@ -25,7 +25,7 @@
 import os
 import sys
 import logging as log
-from openvino.inference_engine import IENetwork, IECore
+from openvino.inference_engine import IENetwork, IEPlugin
 
 
 class Network:
@@ -35,33 +35,112 @@ class Network:
     """
 
     def __init__(self):
-        ### TODO: Initialize any class variables desired ###
+        ### Initialize any class variables desired ###
+        self.net = None
+        self.plugin = None
+        self.input_blob = None
+        self.out_blob = None
+        self.net_plugin = None
+        self.infer_request_handle = None
 
-    def load_model(self):
-        ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
-        ### TODO: Add any necessary extensions ###
-        ### TODO: Return the loaded inference plugin ###
-        ### Note: You may need to update the function parameters. ###
-        return
+    def load_model(self, model, device, input_size, output_size, num_requests, cpu_extension=None, plugin=None):
+        """
+         Loads a network and an image to the Inference Engine plugin.
+        :param model: .xml file of pre trained model
+        :param cpu_extension: extension for the CPU device
+        :param device: Target device
+        :param input_size: Number of input layers
+        :param output_size: Number of output layers
+        :param num_requests: Index of Infer request value. Limited to device capabilities.
+        :param plugin: Plugin for specified device
+        :return:  Shape of input layer
+        """
+
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+        # Plugin initialization for specified device
+        # and load extensions library if specified
+        if not plugin:
+            log.info("Initializing plugin for {} device...".format(device))
+            self.plugin = IEPlugin(device=device)
+        else:
+            self.plugin = plugin
+
+        if cpu_extension and 'CPU' in device:
+            self.plugin.add_cpu_extension(cpu_extension)
+
+        # Read IR
+        log.info("Reading IR...")
+        self.net = IENetwork(model=model_xml, weights=model_bin)
+        log.info("Loading IR to the plugin...")
+
+        if self.plugin.device == "CPU":
+            supported_layers = self.plugin.get_supported_layers(self.net)
+            not_supported_layers = \
+                [l for l in self.net.layers.keys() if l not in supported_layers]
+            if len(not_supported_layers) != 0:
+                log.error("Following layers are not supported by "
+                          "the plugin for specified device {}:\n {}".
+                          format(self.plugin.device,
+                                 ', '.join(not_supported_layers)))
+                log.error("Please try to specify cpu extensions library path"
+                          " in command line parameters using -l "
+                          "or --cpu_extension command line argument")
+                sys.exit(1)
+
+        if num_requests == 0:
+            # Loads network read from IR to the plugin
+            self.net_plugin = self.plugin.load(network=self.net)
+        else:
+            self.net_plugin = self.plugin.load(network=self.net, num_requests=num_requests)
+
+        self.input_blob = next(iter(self.net.inputs))
+        self.out_blob = next(iter(self.net.outputs))
+        assert len(self.net.inputs.keys()) == input_size, \
+            "Supports only {} input topologies".format(len(self.net.inputs))
+        assert len(self.net.outputs) == output_size, \
+            "Supports only {} output topologies".format(len(self.net.outputs))
+
+        return self.plugin, self.get_input_shape()
 
     def get_input_shape(self):
-        ### TODO: Return the shape of the input layer ###
-        return
-
-    def exec_net(self):
+        """
+        Gives the shape of the input layer of the network.
+        :return: None
+        """
+        return self.net.inputs[self.input_blob].shape
+    
+    def exec_net(self, request_id, frame):
         ### TODO: Start an asynchronous request ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        self.infer_request_handle = self.net_plugin.start_async(
+            request_id=request_id, inputs={self.input_blob: frame})
+        return self.net_plugin
 
-    def wait(self):
+
+    def wait(self, request_id):
         ### TODO: Wait for the request to be complete. ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        wait_process = self.net_plugin.requests[request_id].wait(-1)
+        return wait_process
 
-    def get_output(self):
-        ### TODO: Extract and return the output results
-        ### Note: You may need to update the function parameters. ###
-        return
+    def get_output(self, request_id, output=None):
+        """
+        Gives a list of results for the output layer of the network.
+        :param request_id: Index of Infer request value. Limited to device capabilities.
+        :param output: Name of the output layer
+        :return: Results for the specified request
+        """
+        if output:
+            res = self.infer_request_handle.outputs[output]
+        else:
+            res = self.net_plugin.requests[request_id].outputs[self.out_blob]
+        return res
+    
+    def clean(self):
+        
+        del self.net_plugin
+        del self.plugin
+        del self.net
--- main.py
+++ main.py
@@ -27,9 +27,12 @@ import socket
 import json
 import cv2
 
+
+import numpy as np
 import logging as log
 import paho.mqtt.client as mqtt
 
+from collections import deque
 from argparse import ArgumentParser
 from inference import Network
 
@@ -69,10 +72,33 @@ def build_argparser():
 
 
 def connect_mqtt():
-    ### TODO: Connect to the MQTT client ###
-    client = None
+    ### Connect to the MQTT client ###
+    client = mqtt.Client()
+    client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
 
     return client
+
+def model_out(frame, result):
+    """
+    Parse model output.
+    :param frame: frame from camera/video
+    :param result: list contains the data to parse model
+    :return: person count and frame
+    """
+    current_count = 0
+    for obj in result[0][0]:
+        #if obj['class_id'] == 1:
+        # Draw bounding box for object when it's probability is more than
+        #  the specified threshold
+        #print(obj)
+            if obj[2] > prob_threshold:
+                xmin = int(obj[3] * initial_w)
+                ymin = int(obj[4] * initial_h)
+                xmax = int(obj[5] * initial_w)
+                ymax = int(obj[6] * initial_h)
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 55, 255), 1)
+                current_count = current_count + 1
+    return frame, current_count
 
 
 def infer_on_stream(args, client):
@@ -84,37 +110,148 @@ def infer_on_stream(args, client):
     :param client: MQTT client
     :return: None
     """
+    #f = open("inference.txt","x")
+    # Flag for the input image
+    track_len = 12 # for ssd_mobilenet_v2_coco
+    #track_len = 3 # for person-detection-retail-0013
+    buffer = deque(maxlen=track_len)
+    
+    cur_request_id = 0
+    last_count = 0
+    total_count = 0
+    start_time = 0
+    #count = 0
+    #previous_frame = False
+    #previous_previous_frame = False
     # Initialise the class
     infer_network = Network()
     # Set Probability threshold for detections
+    global initial_w, initial_h, prob_threshold
+    
     prob_threshold = args.prob_threshold
+    
+    start_time = time.time()
 
-    ### TODO: Load the model through `infer_network` ###
+    ### Load the model through `infer_network` ###
+    n, c, h, w = infer_network.load_model(args.model, args.device, 
+                                          1, 1, cur_request_id,
+                                          args.cpu_extension)[1]
+    load_time = time.time() - start_time
+    #f = open("mn_v1_inference.txt", "a")  # append mode 
+    #f = open("mn_v2_inference.txt", "a")  # append mode 
+    #f = open("i_v2_inference.txt", "a")  # append mode 
+    f = open("inference.txt","a")
+    
+    f.write("Load time: {}".format(str(load_time)))
+    
+    
 
-    ### TODO: Handle the input stream ###
 
-    ### TODO: Loop until stream is over ###
+    ### Handle the input stream ###
+    # Checks for camera (live) feed
+    single_image_mode = False
+    if args.input == 'CAM':
+        input_stream = 0
+    # Checks for input image
+    elif args.input.endswith('.jpg') or args.input.endswith('.bmp'):
+        single_image_mode = True
+        input_stream = args.input
+    # Checks for video file
+    else:
+        input_stream = args.input
+        assert os.path.isfile(args.input), "Specified input file doesn't exist"
 
-        ### TODO: Read from the video capture ###
+    cap = cv2.VideoCapture(input_stream)
+    
+    if input_stream:
+        cap.open(args.input)
+        
+    if not cap.isOpened():
+        log.error("ERROR! Unable to open video source")
+    
+    
+    initial_w = cap.get(3)
+    initial_h = cap.get(4)
+    ### Loop until stream is over ###
+    start_time = time.time()
+    while cap.isOpened():
 
-        ### TODO: Pre-process the image as needed ###
+        ### Read from the video capture ###
+        flag, frame = cap.read()
+        if not flag:
+            break
+        
+        key_pressed = cv2.waitKey(60)
 
-        ### TODO: Start asynchronous inference for specified request ###
+        ### Pre-process the image as needed ###
+        image = cv2.resize(frame, (w,h))
+        image = image.transpose((2, 0, 1))
+        image = image.reshape((n, c, h, w))
 
-        ### TODO: Wait for the result ###
+        ### Start asynchronous inference for specified request ###
+        inf_start = time.time()
+        infer_network.exec_net(cur_request_id, image)
 
-            ### TODO: Get the results of the inference request ###
-
-            ### TODO: Extract any desired stats from the results ###
-
-            ### TODO: Calculate and send relevant information on ###
+        ### Wait for the result ###
+        #if infer_network.wait() == 0:
+        if infer_network.wait(cur_request_id) == 0:
+            det_time = time.time() - inf_start
+            ### Get the results of the inference request ###
+            result = infer_network.get_output(cur_request_id)
+            ### Extract any desired stats from the results ###
+            frame, current_count = model_out(frame, result)
+            inf_time_message = "Inference time: {:.3f}ms"\
+                               .format(det_time * 1000)
+            cv2.putText(frame, inf_time_message, (15, 15),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)            
+            ### Calculate and send relevant information on ###
             ### current_count, total_count and duration to the MQTT server ###
             ### Topic "person": keys of "count" and "total" ###
             ### Topic "person/duration": key of "duration" ###
+            buffer.append(current_count)
+            if np.sum(buffer) > 0:
+                current_count = 1
+            if current_count > last_count:
+                start_time = time.time()
+                total_count = total_count + current_count - last_count
+                client.publish("person", json.dumps({"total": total_count}))
+                
+            if current_count < last_count:
+                duration = int(time.time() - start_time - det_time*track_len)
+                client.publish("person/duration", json.dumps({"duration": duration}))
+                
+            client.publish("person", json.dumps({"count": current_count}))
+            last_count = current_count
+            
+            if key_pressed == 27:
+                break
+                
+                
 
-        ### TODO: Send the frame to the FFMPEG server ###
+        ### end the frame to the FFMPEG server ###
+        sys.stdout.buffer.write(frame)
+        sys.stdout.flush()
 
-        ### TODO: Write an output image if `single_image_mode` ###
+        ### Write an output image if `single_image_mode` ###
+        #infer_time = time.time() - start_time
+        
+        f.write('\n')
+        f.write(str(det_time * 1000))
+        if single_image_mode:
+            cv2.imwrite('output_image.jpg', frame)
+            
+        #cv2.imwrite("mobilenet_v1/frame%d.jpg" % count,frame)
+        #cv2.imwrite("mobilenet_v2/frame%d.jpg" % count,frame)
+        #cv2.imwrite("inception_v2/frame%d.jpg" % count,frame)
+        #print(count)
+        #count+=1
+        
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    client.disconnect()
+    infer_network.clean()
+    f.close()
 
 
 def main():
@@ -128,6 +265,7 @@ def main():
     # Connect to the MQTT server
     client = connect_mqtt()
     # Perform inference on the input stream
+
     infer_on_stream(args, client)
 
 
